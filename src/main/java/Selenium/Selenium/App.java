8fa99de5188f861f79b7018bbbe776aa0bb959c1
Yajing  Wu <yajingwu@Yajings-MacBook.local>
2017-03-10 21:02:59 -0500

    add database connection

--- src/main/java/Selenium/Selenium/App.java
+++ src/main/java/Selenium/Selenium/App.java
@@ -1,38 +1,78 @@
 package Selenium.Selenium;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
 /**
- * Hello world!
+ * Facebook sign up page test
  *
  */
 public class App 
 {
-    public static void main( String[] args ) throws InterruptedException
+	public static String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug","Sep", "Oct", "Nov","Dec"};
+    public static void main( String[] args ) throws InterruptedException, SQLException
     {
-      
+    	try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("mysql driver is not found!");
+			e.printStackTrace();
+		}
+		String dbUrl = "jdbc:mysql://localhost/test";
+		Connection conn = DriverManager.getConnection(dbUrl, "root", "988527");
+		Statement statement = conn.createStatement();
+         // Result set get the result of the SQL query
+        ResultSet result = statement.executeQuery("select * from Client");
         WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.facebook.com");
-		driver.findElement(By.id("u_0_1")).sendKeys("Yajing");
-		driver.findElement(By.id("u_0_3")).sendKeys("Wu");
-		driver.findElement(By.id("u_0_6")).sendKeys("wuyajing527@gmail.com");
-		driver.findElement(By.id("u_0_9")).sendKeys("wuyajing527@gmail.com");
-		driver.findElement(By.id("u_0_d")).sendKeys("adlslslferw");
-		Thread.sleep(1000);
-		Select dropdown = new Select(driver.findElement(By.id("u_0_f")).findElement(By.id("month")));
-		dropdown.selectByVisibleText("May");
-		dropdown = new Select(driver.findElement(By.id("u_0_f")).findElement(By.id("day")));
-		dropdown.selectByVisibleText("27");
-		dropdown = new Select(driver.findElement(By.id("u_0_f")).findElement(By.id("year")));
-		dropdown.selectByVisibleText("1988");
-	
-		Thread.sleep(1000);
-		driver.findElement(By.id("u_0_k")).click();
-		Thread.sleep(1000);
-		driver.findElement(By.id("u_0_h")).click();
-	    driver.close();
+         while(result.next()){
+        	 String firstName = result.getString("FirstName");
+        	 System.out.println(firstName);
+        	 driver.findElement(By.id("u_0_1")).sendKeys(firstName);
+        	 
+        	 String lastName = result.getString("LastName");
+        	 System.out.println(lastName);
+     		 driver.findElement(By.id("u_0_3")).sendKeys(lastName);
+     		 
+     		 String email = result.getString("Email");
+     		 driver.findElement(By.id("u_0_6")).sendKeys(email);
+     		 driver.findElement(By.id("u_0_9")).sendKeys(email);
+     		 
+     		 String password = result.getString("Password");
+     		 driver.findElement(By.id("u_0_d")).sendKeys(password);
+     		 Thread.sleep(1000);
+     		 
+     		 String date = result.getString("Birth");
+     		 System.out.println(date);
+     		 
+     		 Select dropdown = new Select(driver.findElement(By.id("u_0_e")).findElement(By.id("month")));
+     		 int monthIndex = Integer.parseInt(date.substring(5,7))-1;
+     		 dropdown.selectByVisibleText(months[monthIndex]);
+     		 dropdown = new Select(driver.findElement(By.id("u_0_e")).findElement(By.id("day")));
+     		 dropdown.selectByVisibleText(date.substring(8));
+     		 dropdown = new Select(driver.findElement(By.id("u_0_e")).findElement(By.id("year")));
+     		 dropdown.selectByVisibleText(date.substring(0, 4));
+     		 Thread.sleep(1000);
+     		 
+     		 String sex= result.getString("Sex");
+     		 if(sex.equals("Female")){
+     			 driver.findElement(By.id("u_0_k")).click();
+     		 }else{
+     			 driver.findElement(By.id("u_0_l")).click();
+     		 }		
+     		 Thread.sleep(1000);
+     		 driver.findElement(By.id("u_0_h")).click();
+         }
+         conn.close();
+         driver.close();
     }
 }
